3319ad883f2e86151be4724516e3f6d859c65681
Bane Kimoun <f.cobos@it-students.fr>
2019-01-03 15:37:21 +0100

    add options

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //JavaScript natif
 document.querySelector('.button').addEventListener('click', function(){
 
-pokemon = function getPokByNumber($number){
+let pokemon = function getPokByNumber($number){
     fetch('https://pokeapi.co/api/v2/pokemon/' + $number + '/')
     //pokemon = function getPokByNumber(number){
     //fetch(`https://pokeapi.co/api/v2/pokemon/${number}/`)
@@ -16,8 +16,33 @@ pokemon(6);
   
 });
 
+document.querySelector('.magic').addEventListener('click', function(){
+   let cards = function getCards($number){
+    fetch("http://api.magicthegathering.io/v1/cards?page="+ $number + '/')
+    .then(function(response){ 
+        return response.json();
+        })
+        .then(function(boon){
+           let cartes = boon.cards
+           cartes.forEach(function(element) {
+            document.querySelector('.global').innerHTML += '<div class= "cartes">' +  '<h1>' + element.name + '</h1>'
+            + '<img src= "' + element.imageUrl + '">' + "<p>" + element.type + "</p>" + '</div>'
+             
+          });
+          
+           
+            
+        })
+    }
+    cards(2);
+})
+
 //JQUERY
 
-$(document).ready(function(){
-    
+$(document).ready(function(){ // on attend que la page soit entierement chargé pour faire la suite
+    $('.buttonBis').click(function(){
+        $.get('https://pokeapi.co/api/v2/pokemon/7/', function(data){
+           $('.pokeName').html(data.name);
+        })
+    })
 })
