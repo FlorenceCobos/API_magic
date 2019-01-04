46f268e6a4e832e6aea39e3f784a503671c2c738
Bane Kimoun <f.cobos@it-students.fr>
2019-01-04 13:15:36 +0100

    add options

--- script.js
+++ script.js
@@ -26,17 +26,37 @@ document.querySelector('.magic').addEventListener('click', function(){
            let cartes = boon.cards
            document.querySelector('.global').innerHTML=""
            cartes.forEach(function(element) {
-            document.querySelector('.global').innerHTML += '<div class= "cartes">' +  '<h1>' + element.name + '</h1>'
-            + '<img src= "' + element.imageUrl + '">' + "<p>" + element.type + "</p>" + '</div>'
+            let id = element.id;
+            document.querySelector('.global').innerHTML += '<div class= "cartes">' + '<img class= "like" data-id="' + id + '" src= "./like.svg">'+  '<h1>' +  element.name +  '</h1>'  +
+             '<img src= "' + element.imageUrl + '">' + "<p>" + element.type + "</p>" + '</div>'
              
-          });
-          
-           
-            
+
+          });      
         })
     }
     cards(2);
 })
+
+if(deck == null){
+    var deck = [];
+    document.querySelector('.global').addEventListener('click', function(el){
+        let target = el.target;
+        if (target.classList.contains('like') == true){
+            let id = el.target.dataset.id;
+            
+            deck.push(id);
+            localStorage.setItem("deck",JSON.stringify(deck));
+            console.log(deck);
+            //let liked = [element.name];
+            // let favoriteCards = localStorage.setItem('favoriteCards', liked);
+            // console.log(favoriteCards);
+        }
+    })
+}else{
+
+}
+
+
 document.querySelector('.magic2').addEventListener('click', function(){
    let cards = function getCards($number){
     fetch("https://api.magicthegathering.io/v1/cards?page="+ $number + '/')
@@ -58,6 +78,9 @@ document.querySelector('.magic2').addEventListener('click', function(){
     }
     cards(3);
 })
+
+
+
 
 
 
